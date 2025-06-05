844fd3f21380c74dbeb8528c59153645ac7d4d4e
Sangram Majhi <155440916+yujiroxangram@users.noreply.github.com>
2025-06-06 03:05:50 +0530

    JPA Annotations
    Annoted bibliography!!!

---
+++ sb_april/JPA_ANNOTATIONS/src/main/java/com/example/JPA_ANNOTATIONS/JpaAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPA_ANNOTATIONS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAnnotationsApplication.class, args);
+	}
+
+}
---
+++ sb_april/JPA_ANNOTATIONS/src/test/java/com/example/JPA_ANNOTATIONS/JpaAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPA_ANNOTATIONS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
